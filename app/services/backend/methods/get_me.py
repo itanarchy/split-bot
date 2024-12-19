9f4237f8c28b087f9b9482b6daa3e59c5248ee53
wakaree <svegliatixanax42@gmail.com>
2024-12-19 18:42:00 +0100

    fix: replace getMe method path

--- app/services/backend/methods/get_me.py
+++ app/services/backend/methods/get_me.py
@@ -7,7 +7,7 @@ from .base import SplitMethod
 class GetMe(
     SplitMethod[User],
     http_method=HTTPMethod.GET,
-    api_method="/user/info",
+    api_method="/user/get",
     returning=User,
     response_data_key=["user"],
 ):
